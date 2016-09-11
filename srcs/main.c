f76aa1d930374ea447822b395f6c977f08c61b88
Djamel BENDAOUD <dbendaou@e2r3p2.42.fr>
2016-09-11 19:26:03 +0200

    merge

--- srcs/main.c
+++ srcs/main.c
@@ -5,98 +5,53 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dbendaou <dbendaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/09/07 16:13:57 by dbendaou          #+#    #+#             */
-/*   Updated: 2016/09/11 19:09:11 by dbendaou         ###   ########.fr       */
+/*   Created: 2016/09/06 16:49:36 by dbendaou          #+#    #+#             */
+/*   Updated: 2016/09/11 19:25:50 by dbendaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int 	ft_key_funct(int keycode)
+int 	ft_mouse_funct(int button, int x, int y, void *mlx)
 {
-	t_env	env;
-	ft_putnbr(keycode);
+	return (0);
+}
+
+int		ft_key_funct(int keycode, void *mlx)
+{
+
+	printf("key event %d\n", keycode);
 	if (keycode == 53)
 	{
-		ft_putstr("Trying to exit.....\n");
-		exit (0);
+		printf("Weshbolosse\n");
+		exit (1);
 	}
-	return (0);
+	return (1);
 }
 
-int 	ft_mouse_funct()
-{
-	ft_putstr("CLICK\n");
-	return (0);
-}
 
-void 	*ft_create(void *mlx, void *win)
+int		main(int ac, char **av)
 {
 	t_env	env;
+	int		x;
+	int		y;
 
-	env.height = 420;
-	env.width = 420;
-	mlx = mlx_init();
-	win = mlx_new_window(mlx, env.width, env.height, "Titre");
-	printf(" mlx = %s\n win = %s\n",mlx, win);
-/*	if (env.mlx == NULL)
-	{
-		ft_putstr(E_mlx_init);
-		return (1);
-	}
-	if (env.win == NULL)
-	{
-		ft_putstr(E_mlx_win);
-		return (1);
-	}
-	else*/
-	env.win = win;
-		return (mlx);
-}
-
-void	ft_pixel_put()
-{
-	t_env	env;
-	int x;
-	int y;
-	void *tmp;
-	void *tmpwin;
-	tmpwin = env.win;
-	tmp = env.mlx;
-	printf("2mlx = %s\n win = %s\n",env.mlx, env.win);
+	env.mlx = mlx_init();
+	env.win = mlx_new_window(env.mlx, 420, 420, "42");
 	y = 50;
 	while (y < 150)
 	{
 		x = 50;
 		while (x < 150)
 		{
-			mlx_pixel_put(tmp, tmpwin, x, y, BLUE);
+			mlx_pixel_put(env.mlx, env.win, x, y, BLUE);
 			x++;
 		}
-		printf("rien\n");
 		y++;
 	}
-
-}
-
-int		main(int ac, char **av)
-{
-
-	t_env	env;
-	void	*tmpwin;
-	void	*tmpmlx;
-
-
-	printf("mlx = %s\nwin = %s\n",env.mlx, env.win);
-	tmpmlx = ft_create(env.mlx, env.win);		
-	printf("3mlx = %s\n win = %s\n",tmpmlx, env.win);
-	ft_pixel_put();
-	ft_putstr("ouais bof\n");
-//	mlx_key_hook(env.win, ft_key_funct, env.mlx);
-//	mlx_mouse_hook(env.win, ft_mouse_funct, env.mlx);
-//	mlx_loop(tmpmlx);
-	printf("4mlx = %s\n win = %s\n",tmpmlx, env.win);
-	ft_putstr("seg loop none"); 
-	
+	mlx_mouse_hook(env.win, ft_mouse_funct, env.mlx);
+	mlx_key_hook(env.win, ft_key_funct, env.mlx);
+	mlx_loop(env.mlx);
 	return (0);
+
 }
