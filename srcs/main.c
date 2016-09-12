79b301c465a16e67a299e7c02a5b7a30c790f35e
Djamel BENDAOUD <dbendaou@e2r11p11.42.fr>
2016-09-12 18:34:26 +0200

    affiche la map

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: dbendaou <dbendaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/07 16:13:57 by dbendaou          #+#    #+#             */
-/*   Updated: 2016/09/12 17:18:07 by dbendaou         ###   ########.fr       */
+/*   Updated: 2016/09/12 18:32:23 by dbendaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ int 	ft_mouse_funct(int button, int x, int y, void *param)
 	return (0);
 }
 
-t_env 	*ft_create()
+t_env 	ft_create()
 {
 	t_env	env;
 
@@ -36,7 +36,7 @@ t_env 	*ft_create()
 	env.width = 420;
 	env.mlx = mlx_init();
 	env.win = mlx_new_window(env.mlx, env.width, env.height, TITLE);
-	return(&env);
+	return(env);
 }
 
 void	ft_pixel_put()
@@ -72,13 +72,41 @@ void	ft_loop(t_env env)
 int		main(int ac, char **av)
 {
 	t_env	env;
-	int i = 0;
-	char *tmp;
-	
-	env = *ft_create();
+	int 	i = 0;
+	char 	*tmp;
+	int 	fd;
+
+	fd = open(av[1], O_RDONLY);
+	printf("%d\n", fd);
+	while ((i = read(fd, &tmp, 19) > 0))
+	{
+		printf("%s\n", &tmp);
+	}
+	env = ft_create();
 	ft_pixel_put();
 	ft_putstr("ouais bof\n");
 	ft_loop(env);
 		
 	return (0);
 }
+/*
+int i;
+FILE *fp;
+int c;
+
+	fp = fopen(argv[i], "r");
+	if(fp == NULL)
+		{
+		fprintf(stderr, "cat: can't open %s\n", argv[i]);
+		continue;
+		}
+
+	while((c = getc(fp)) != EOF)
+		putchar(c);
+
+	fclose(fp);
+
+f=open(argv[1],O_RDONLY);
+while ((n=read(f,l,80)) > 0)
+    write(1,l,n);
+    */
